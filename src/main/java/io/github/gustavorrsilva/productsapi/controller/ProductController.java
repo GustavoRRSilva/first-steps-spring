84b44d45ffd58eb52a4baaf410357fb617f60e54
GustavoRRSilva <gstvrrds@gmail.com>
2025-08-21 12:11:37 -0300

    feat:finding product by id

--- src/main/java/io/github/gustavorrsilva/productsapi/controller/ProductController.java
+++ src/main/java/io/github/gustavorrsilva/productsapi/controller/ProductController.java
@@ -2,11 +2,9 @@ package io.github.gustavorrsilva.productsapi.controller;
 
 import io.github.gustavorrsilva.productsapi.model.Product;
 import io.github.gustavorrsilva.productsapi.repository.ProductRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -28,5 +26,10 @@ public class ProductController {
            productRepository.save(product);
            return product;
 
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable("id") String id){
+        return productRepository.findById(id).orElse(null);//optional because we don't know if there is a product with this id
     }
 }
--- src/main/java/io/github/gustavorrsilva/productsapi/repository/ProductRepository.java
+++ src/main/java/io/github/gustavorrsilva/productsapi/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import io.github.gustavorrsilva.productsapi.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,String> {
+
 }
